f711f7938c3d306c3dd0c738219c8cdbf30e81f9
Nazar Tymiv <nazartumiv@gmail.com>
2023-11-02 12:18:42 +0100

    Created useEffect and created loading component. Exercise compoleted :)

--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,17 +1,20 @@
-function SelectTypeForm(props) {
-  // Write code here...
+function SelectTypeForm({ setDataType }) {
+    const handleDataType = (e) => setDataType(e.target.value);
 
-  return (
-    <form className="three-column-grid__expand-two gap-md">
-      <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
-        <option value="">Please select...</option>
-        <option value="people">People</option>
-        <option value="planets">Planets</option>
-        <option value="starships">Starships</option>
-      </select>
-    </form>
-  );
+    return (
+        <form
+            className="three-column-grid__expand-two gap-md"
+            onChange={handleDataType}
+        >
+            <label htmlFor="type">Data Type</label>
+            <select id="type" name="type">
+                <option value="">Please select...</option>
+                <option value="people">People</option>
+                <option value="planets">Planets</option>
+                <option value="starships">Starships</option>
+            </select>
+        </form>
+    );
 }
 
 export default SelectTypeForm;
