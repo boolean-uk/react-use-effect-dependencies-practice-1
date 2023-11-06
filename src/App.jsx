09d58ea6dfe8872b819a1d97a93e151dddffb45a
Loza_TM <lozatmamo@gmail.com>
2023-11-06 19:13:18 +0100

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react"; // Import useEffect
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
   console.log({ data });
 
-  // Write code here.
-  //
+  useEffect(() => {
+    if (dataType) {
+      fetch(`https://swapi.dev/api/${dataType}/`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, [dataType]);
 
   return (
     <div>
