897ca85c0ae1f4ab504dc28cd1411db49f3e3d72
Pia Loana Mullenbach <pia.loana.mullenbach@dk.experis.com>
2024-02-27 10:38:28 +0100

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
   console.log({ data });
 
   // Write code here.
-  //
+  useEffect(() => {
+    console.log(
+      "Running my effect each time the dependencies are updatred; note: dependency array contains [newTaskText]..."
+    );
+    const TODOS_ENDPOINT = `https://swapi.dev/api/${dataType}/`;
+    fetch(TODOS_ENDPOINT)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("received data:", data);
+        console.log("setting new state....");
+        setData(data.results);
+      });
+  }, [dataType]);
 
   return (
     <div>
       <SelectTypeForm setDataType={setDataType} />
-      {data && <DataList dataType={dataType} data={data.results} />}
+      {data && <DataList dataType={dataType} data={data} />}
     </div>
   );
 }
