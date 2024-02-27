253f61c11d249b4a570347111854fe770d8a41c0
Linus Willmont <linus.willmont@se.experis.com>
2024-02-27 09:54:58 +0100

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
-  const [dataType, setDataType] = useState("");
+	const [dataType, setDataType] = useState("");
 
-  const [data, setData] = useState(null);
+	const [data, setData] = useState(null);
 
-  console.log({ data });
+	console.log({ data });
 
-  // Write code here.
-  //
+	// Write code here.
+	//
+	useEffect(() => {
+		if (dataType === "") return;
 
-  return (
-    <div>
-      <SelectTypeForm setDataType={setDataType} />
-      {data && <DataList dataType={dataType} data={data.results} />}
-    </div>
-  );
+		fetch(`https://swapi.dev/api/${dataType}/`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Fetched data:", data);
+				setData(data);
+			})
+			.catch((error) => console.error("Failed to get data:", error));
+	}, [dataType]);
+
+	return (
+		<div>
+			<SelectTypeForm setDataType={setDataType} />
+			{data && <DataList dataType={dataType} data={data.results} />}
+		</div>
+	);
 }
--- src/components/SelectTypeForm.jsx
+++ src/components/SelectTypeForm.jsx
@@ -1,17 +1,23 @@
-function SelectTypeForm(props) {
-  // Write code here...
+function SelectTypeForm({ setDataType }) {
+	// Write code here...
+	const handleSelectType = (e) => {
+		setDataType(e.target.value);
+	};
 
-  return (
-    <form className="three-column-grid__expand-two gap-md">
-      <label htmlFor="type">Data Type</label>
-      <select id="type" name="type">
-        <option value="">Please select...</option>
-        <option value="people">People</option>
-        <option value="planets">Planets</option>
-        <option value="starships">Starships</option>
-      </select>
-    </form>
-  );
+	return (
+		<form
+			className="three-column-grid__expand-two gap-md"
+			onChange={handleSelectType}
+		>
+			<label htmlFor="type">Data Type</label>
+			<select id="type" name="type">
+				<option value="">Please select...</option>
+				<option value="people">People</option>
+				<option value="planets">Planets</option>
+				<option value="starships">Starships</option>
+			</select>
+		</form>
+	);
 }
 
 export default SelectTypeForm;
