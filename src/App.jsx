6a4b14e2a7938c2e08feb44069dcc3faa822c3a6
spectraldesign <spectraldesign1@gmail.com>
2024-02-28 09:01:11 +0100

    Core

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
-  console.log({ data });
-
-  // Write code here.
-  //
+  useEffect(() => {
+    if (dataType) {
+      fetch(`https://swapi.dev/api/${dataType}/`)
+        .then((response) => response.json())
+        .then((data) => {
+          setData(data);
+        });
+    }
+    else {
+      setData(null);
+    }
+  }, [dataType]);
 
   return (
     <div>
