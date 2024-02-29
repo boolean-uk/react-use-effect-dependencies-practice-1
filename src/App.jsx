920fb3441e8aca5b4793ccca255d4df68eddb702
Martin Skivenesvåg Johannessen <martin.johannessen@no.experis.com>
2024-02-29 09:42:36 +0100

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import DataList from "./components/DataList.jsx";
 import SelectTypeForm from "./components/SelectTypeForm.jsx";
 import "./App.css";
 
 export default function App() {
   const [dataType, setDataType] = useState("");
-
   const [data, setData] = useState(null);
 
   console.log({ data });
 
-  // Write code here.
-  //
+  useEffect(() => {
+    if (dataType)
+    {
+      const fetchData = async () => {
+        try
+        {
+          const response = await fetch(`https://swapi.dev/api/${dataType}/`);
+          if (!response.ok)
+          {
+            throw new Error(`HTTP error! status: ${response.status}`);
+          }
+          const jsonData = await response.json();
+          setData(jsonData);
+        } catch (error)
+        {
+          console.error("Fetch error: ", error);
+        }
+      };
+
+      fetchData();
+    }
+  }, [dataType]);
 
   return (
     <div>
